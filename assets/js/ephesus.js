363134d0f821513ee033ed4832d13b3488ee62d5
craig-shenton <craig.shenton@nhs.net>
2022-04-22 15:36:19 +0100

    sort dark mode as default

--- assets/js/ephesus.js
+++ assets/js/ephesus.js
@@ -1,12 +1,12 @@
 function toggleNightMode(){
-	if(document.documentElement.getAttribute('data-theme') == 'light'){
-		document.documentElement.setAttribute('data-theme', 'dark');
-		document.getElementById('mode-switcher').classList.add('active');
-		localStorage.setItem("theme","");
+	if(document.documentElement.getAttribute('data-theme') == 'dark'){
+		document.documentElement.setAttribute('data-theme', 'light');
+		document.getElementById('mode-switcher').classList.remove('active');
+		localStorage.setItem("theme","light");
 	}
 	else{
 		document.documentElement.setAttribute('data-theme', 'dark');
-		document.getElementById('mode-switcher').classList.remove('active');
-		localStorage.setItem("theme","light");
+		document.getElementById('mode-switcher').classList.add('active');
+		localStorage.setItem("theme","dark");
 	}
 }
