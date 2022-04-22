7e9eb99b7161e9da99ed6137ef84c670d5933e7d
craig-shenton <craig.shenton@nhs.net>
2022-04-22 14:52:18 +0100

    dark mode

--- assets/js/ephesus.js
+++ assets/js/ephesus.js
@@ -1,11 +1,11 @@
 function toggleNightMode(){
-	if(document.documentElement.getAttribute('data-theme') == 'dark'){
-		document.documentElement.setAttribute('data-theme', 'light');
+	if(document.documentElement.getAttribute('data-theme') == 'light'){
+		document.documentElement.setAttribute('data-theme', 'dark');
 		document.getElementById('mode-switcher').classList.add('active');
-		localStorage.setItem("theme","light");
+		localStorage.setItem("theme","dark");
 	}
 	else{
-		document.documentElement.setAttribute('data-theme', 'dark');
+		document.documentElement.setAttribute('data-theme', 'light');
 		document.getElementById('mode-switcher').classList.remove('active');
 		localStorage.setItem("theme","");
 	}
